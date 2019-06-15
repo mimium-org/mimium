077c00a39224da00551c274a1ed50409449e6de0
tomoyanonymous <anon.spyc@gmail.com>
2019-06-15 21:33:56 +0900

    test for function

--- test/interpreter_test.cpp
+++ test/interpreter_test.cpp
@@ -18,8 +18,9 @@ TEST(interpreter_test, assign2) {
      std::string teststr1 = "main = 1.245";
      driver.parsestring(teststr1);
      interpreter.loadAst(driver.getMainAst());
-    double res = interpreter.findVariable("main");
-     EXPECT_EQ(res,1.245);
+    mValue res = interpreter.findVariable("main");
+    double resv = mimium::Interpreter::get_as_double(res);
+     EXPECT_EQ(resv,1.245);
 }
 
 
@@ -29,6 +30,20 @@ TEST(interpreter_test, assignexpr) {
      std::string teststr2 = "main = 1+2.3/2.5*(2-1)+100";
      driver.parsestring(teststr2);
      interpreter2.loadAst(driver.getMainAst());
-    double res = interpreter2.findVariable("main");
-     EXPECT_EQ(res,101.92);
+    mValue res = interpreter2.findVariable("main");
+     double resv = mimium::Interpreter::get_as_double(res);
+
+     EXPECT_EQ(resv,101.92);
+     } 
+
+TEST(interpreter_test, assignfunction) {
+     mmmpsr::MimiumDriver driver;
+     mimium::Interpreter interpreter2;
+     std::string teststr2 = "fun(a,b) = a*b+1 \n main = fun(7,5)";
+     driver.parsestring(teststr2);
+     interpreter2.loadAst(driver.getMainAst());
+    mValue res = interpreter2.findVariable("main");
+     double resv = mimium::Interpreter::get_as_double(res);
+
+     EXPECT_EQ(resv,36);
      } 
