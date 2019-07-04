a0bb1404f607d0c20dbb218b9f45fec6e6f4afb8
tomoyanonymous <anon.spyc@gmail.com>
2019-07-04 14:53:30 +0900

    typo

--- src/closure.cpp
+++ src/closure.cpp
@@ -5,7 +5,7 @@ std::string Closure::to_string(){
     std::stringstream ss;
     ss << "Closure:<";
     fun->to_string(ss);
-    ss << " , " << env->getName(); 
+    ss << "> , " << env->getName(); 
     return ss.str();
 }
 
