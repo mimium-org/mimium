d018119ba89f9f64ec5de9be45b46c9c578b5806
tomoyanonymous <anon.spyc@gmail.com>
2019-07-17 17:52:54 +0900

    signal handling

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,31 @@
  */
 #include <string>
 #include <unistd.h>
+#include <signal.h>
 #include "helper_functions.hpp"
 #include "driver.hpp"
 
 #include "interpreter.hpp"
 
 
+std::function<void(int)> shutdown_handler;
+void signal_handler(int signo){
+    shutdown_handler(signo);
+}
+
+
 int main(int argc,char *argv[]) {
+    signal(SIGINT,signal_handler);
     mimium::Logger::current_report_level = mimium::Logger::DEBUG;
     auto interpreter =  std::make_unique<mimium::Interpreter>();
+    shutdown_handler = [&interpreter](int signal){
+        if(interpreter->isrunning()){
+        interpreter->stop();
+        }
+        std::cerr << std::endl << "Interuppted by key" << std::endl;
+        exit(0);
+    };
+
     interpreter->init();
     interpreter->add_scheduler();
     if(argc==1){
@@ -24,15 +40,20 @@ int main(int argc,char *argv[]) {
     interpreter->clearDriver();  
     mValue res = interpreter->loadSource(line);
     std::string resstr = mimium::Interpreter::to_string(res);
-    std::cout << resstr << std::endl;
+    // std::cout << resstr << std::endl;
     }
     }
     else{
+        try{
         std::cout << argv[1]<<std::endl;
         mValue res = interpreter->loadSourceFile(argv[1]);
         interpreter->start();
-        sleep(20);
+        while(true){sleep(20);}; //todo : what is best way to wait infinitely? thread?
+        }catch(std::exception& e){
+        std::cerr << e.what()<<std::endl;
         interpreter->stop();
+        interpreter.reset();
+        }
     }
     return 0;
 }
