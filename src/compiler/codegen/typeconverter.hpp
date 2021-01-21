5a35d28cf79230e0730bf64ca6ab1a943989505b
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2021-01-21 22:02:32 +0900

    made typeconverter const

--- src/compiler/codegen/typeconverter.hpp
+++ src/compiler/codegen/typeconverter.hpp
@@ -16,19 +16,19 @@ struct TypeConverter {
   std::unordered_map<std::string, llvm::Type*> aliasmap;
   static void error() { throw std::runtime_error("Invalid Type"); }
 
-  llvm::Type* operator()(types::None& i);
-  llvm::Type* operator()(types::TypeVar& i);
-  llvm::Type* operator()(types::Void& i);
-  llvm::Type* operator()(types::Float& i);
-  llvm::Type* operator()(types::String& i);
-  llvm::Type* operator()(types::Ref& i);
-  llvm::Type* operator()(types::Pointer& i);
-  llvm::Type* operator()(types::Function& i);
-  llvm::Type* operator()(types::Closure& i);
-  llvm::Type* operator()(types::Array& i);
-  llvm::Type* operator()(types::Struct& i);
-  llvm::Type* operator()(types::Tuple& i);
-  llvm::Type* operator()(types::Alias& i);
+  llvm::Type* operator()(types::None const& i);
+  llvm::Type* operator()(types::TypeVar const& i);
+  llvm::Type* operator()(types::Void const& i);
+  llvm::Type* operator()(types::Float const& i);
+  llvm::Type* operator()(types::String const& i);
+  llvm::Type* operator()(types::Ref const& i);
+  llvm::Type* operator()(types::Pointer const& i);
+  llvm::Type* operator()(types::Function const& i);
+  llvm::Type* operator()(types::Closure const& i);
+  llvm::Type* operator()(types::Array const& i);
+  llvm::Type* operator()(types::Struct const& i);
+  llvm::Type* operator()(types::Tuple const& i);
+  llvm::Type* operator()(types::Alias const& i);
 
  private:
   [[nodiscard]] std::string consumeAlias();
