01ad4b645b9cba1c400054997d670fe1dd502c5f
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2021-04-06 02:25:50 +0900

    made higer abstraction for ast classes

--- src/basic/ast_new.hpp
+++ src/basic/ast_new.hpp
@@ -4,122 +4,146 @@
 #include "type.hpp"
 
 namespace mimium {
-namespace ast_defs {
 
-template <class T>
-using IdType = T;
+template <template <class> class C>
+struct MakeRecursive {
+  using asts = C<Box<MakeRecursive>>;
+  using type = typename asts::Expr;
+};
 
-}
-namespace ast {
-template<class T>
-struct Primitive{
+template <class EXPR>
+struct Ast {
+  template <class T, int ID = 0>
+  struct Primitive {
     T v;
-};
+  };
 
-using FloatLit =Primitive<float>;
-using IntLit =Primitive<int>;
-using BoolLit = Primitive<bool>;
-using StringLit = Primitive<std::string>;
+  using FloatLit = Primitive<float>;
+  using IntLit = Primitive<int>;
+  using BoolLit = Primitive<bool>;
+  using StringLit = Primitive<std::string>;
+  using Symbol = Primitive<std::string, 1>;
 
-struct Symbol{
+  template <template <class...> class Category, class T, int ID = 0>
+  struct Aggregate {
+    using type = Category<T>;
+    type v;
+  };
+
+  template <class T>
+  using List = std::list<T>;
+
+  using TupleLit = Aggregate<List, EXPR>;
+  using ArrayLit = Aggregate<List, EXPR, 1>;
+  struct StructKey {
+    std::string key;
+    EXPR v;
+  };
+  using StructLit = Aggregate<List, StructKey>;
+
+  struct Id {
     std::string v;
-};
-template <class EXPR>
-struct TupleLit;
-template <class EXPR>
-struct StructLit;
-template <class EXPR>
-struct ArrayLit;
-template <class EXPR>
-struct Lambda;
-template <class EXPR>
-struct Let;
-template <class EXPR>
-struct If;
-// clang-format off
-template <typename Expr,typename... T>
+  };
+  template <class T>
+  struct LambdaCategory {
+    List<Id> args;
+    T body;
+  };
+  using Lambda = typename Aggregate<LambdaCategory, EXPR>::type;
+  template <class T>
+  struct LetCategory {
+    std::string id;
+    T expr;
+    T body;
+  };
+  using Let = typename Aggregate<LetCategory, EXPR>::type;
+  template <class T>
+  struct IfCategory {
+    T cond;
+    T vthen;
+    std::optional<T> velse;
+  };
+  using If = typename Aggregate<IfCategory, EXPR>::type;
+
+  // clang-format off
+template <typename... Addtionals>
 using ExprBase = std::variant<FloatLit, 
                                 IntLit, 
                                 BoolLit, 
                                 StringLit, 
                                 Symbol, 
-                                TupleLit<Expr>, 
-                                StructLit<Expr>,
-                                ArrayLit<Expr>, 
-                                Lambda<Expr>, 
-                                If<Expr>, 
-                                T...>;
-// clang-format on
+                                TupleLit, 
+                                StructLit,
+                                ArrayLit,
+                                Lambda, 
+                                If, 
+                                Addtionals...>;
+  // clang-format on
+  template <class T>
+  struct AppCategory {
+    T callee;
+    List<EXPR> args;
+  };
+  using App = typename Aggregate<AppCategory, EXPR>::type;
 
-template <class EXPR>
-struct App;
-
-template <class EXPR>
-using ExprPrim = ExprBase<App<EXPR>, Let<EXPR>>;
-
-struct ExprProto{
-    using type = ExprPrim<Box<ExprProto>>;
+  // the Ast struct has to declare Expr type to use in MakeRecursive Container.
+  using Expr = ExprBase<App, Let>;
 };
-using Expr = ExprProto::type;
+using ExprPrim = MakeRecursive<Ast>;
 
-
-template <class EXPR>
-struct App {
-  EXPR callee;
-  std::list<EXPR> args;
-};
-
-template <class EXPR>
-struct TupleLit{
-
-}; 
-template <class EXPR>
-struct StructLit{
-
-}; 
-template <class EXPR>
-struct ArrayLit{
-
-}; 
-template <class EXPR>
-struct Lambda{
-
-}; 
-template <class EXPR>
-struct Let{
-
-}; 
-template <class EXPR>
-struct If{
-     EXPR e;
-}; 
-
-
-Expr e{FloatLit{0}};
-auto& test = std::get<FloatLit>(e);
+auto lit = ExprPrim::asts::FloatLit{0.0};
+ExprPrim::type e{lit};
+auto& test = std::get<ExprPrim::asts::FloatLit>(e);
 
 // HIGH-LEVEL AST including Syntactic Sugar
+template <class EXPR>
+struct Hast {
+  template <template <class...> class T, class U, int ID = 0>
+  using Aggregate = ExprPrim::asts::Aggregate<T, U, ID>;
 
-struct AppCurry;
+  struct CurryPlaceHolder {};
+  using CurryArg = std::variant<EXPR, CurryPlaceHolder>;
+  using App = typename Ast<CurryArg>::App;
+  struct Op {
+    std::string v;
+  };
+  template <class T>
+  struct InfixCategory {
+    Op op;
+    T lhs;
+    T rhs;
+  };
 
-struct Infix;
-struct Schedule;  //@ operator
-struct EnvVar;    // #hoge
-struct Block;
+  using Infix = typename Aggregate<InfixCategory, EXPR>::type;
+  template <class T>
+  struct ScheduleCategory {
+    App expr;
+    T time;
+  };
+  using Schedule = typename Aggregate<ScheduleCategory, EXPR>::type;
+  struct EnvVar {
+    std::string id;
+  };
+  template <class T>
+  struct AssignmentProto {
+    std::string id;
+    T expr;
+  };
+  using Assignment = AssignmentProto<EXPR>;
+  template <class T>
+  using LCategory = ExprPrim::asts::LambdaCategory<T>;
+  using DefFnRvalue = typename Aggregate<LCategory, EXPR>::type;
+  using DefFn = AssignmentProto<DefFnRvalue>;
 
-using ExprWithSugar = ExprBase<Box<Infix>, Box<Schedule>, Box<EnvVar>, Box<AppCurry>, Box<Block>>;
+  using Statements = std::variant<Assignment, DefFn, ExprPrim::asts::App>;
 
-// STATEMENTS
+  //   using ExtFun;TODO
+  using Block = Aggregate<std::list, Statements>;
+  using Expr = ExprPrim::asts::ExprBase<Infix, Schedule, EnvVar, App, Block>;
+};
 
-struct Assignment;
-struct DefFn;
-struct ExtFun;
-
-using Statement = std::variant<Box<Assignment>, Box<DefFn>, Box<ExtFun>>;
-using Statements = std::list<Statement>;
+using HExpr = MakeRecursive<Hast>;
 
 struct TypeAlias;  // type name = hoge
-
-}  // namespace ast
 
 }  // namespace mimium
