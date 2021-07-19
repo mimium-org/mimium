9932da95746a0890ab06ce812df0faaab1eb79ea
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2021-07-19 17:13:53 +0900

    fixed X expression codes

--- src/basic/sexpr.hpp
+++ src/basic/sexpr.hpp
@@ -6,31 +6,18 @@ template <class S, class SymbolT>
 auto makeSExpr(SymbolT const& s);
 
 struct SExpr {
-  std::string x;
-  std::optional<Box<SExpr>> xs;
+  using Leaf = std::variant<Box<SExpr>, std::string>;
+  Leaf x;
+  std::optional<Leaf> xs;
 };
 
 inline auto cons(std::string const& s, SExpr const& s2) {
-  auto res = SExpr{s, std::nullopt};
-  res.xs = std::optional(s2);
-  return res;
+  return SExpr{s, std::optional(s2)};
+  ;
 }
-inline std::optional<Box<SExpr>>* getEnd(SExpr s) {
-  auto* xs = &s.xs;
-  while (xs->has_value()) { xs = &xs->value().getraw().xs; }
-  return xs;
-}
-
-inline auto cons(SExpr s1, SExpr s2) {
-  auto* s1end = getEnd(s1);
-  *s1end = std::optional(s2);
-  return s1;
-}
+inline auto cons(SExpr s1, SExpr s2) { return SExpr{s1, std::optional(s2)}; }
 
 inline auto makeSExpr(std::string s) { return SExpr{s, std::nullopt}; }
-
-template <class iter>
-std::optional<Box<SExpr>> makeSExpr(iter&& x, iter&& end);
 
 template <class iter>
 std::optional<Box<SExpr>> makeSExpr(iter&& x, iter&& end) {
@@ -46,18 +33,21 @@ inline auto makeSExpr(std::initializer_list<std::string> list) {
   return makeSExpr(list.begin(), list.end()).value().getraw();
 }
 
-
 inline auto& head(SExpr const& s) { return s.x; }
 inline auto& tail(SExpr const& s) { return s.xs; }
-
-inline auto toString(std::string const& s) { return s; }
+inline std::string toString(SExpr const& s);
+inline std::string toString(SExpr::Leaf const& leaf) {
+  return std::visit(overloaded{[](std::string const& s) { return s; },
+                               [](Box<SExpr> const& s) { return toString(s.getraw()); }},
+                    leaf);
+}
 
 inline std::string toString(SExpr const& s) {
-  std::string res("(");
-  res += toString(s.x);
-  if (auto t = tail(s)) { res += toString(t.value()); }
-  res += ")";
-  return res;
+  if (s.xs.has_value()) {
+    std::string tail_s = toString(s.xs.value());
+    return "(" + toString(head(s)) + " " + tail_s + ")";
+  }
+  return toString(head(s));
 }
 
 }  // namespace mimium
